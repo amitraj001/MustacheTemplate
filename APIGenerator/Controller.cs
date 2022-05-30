46eb3143b487598ffaceb932f2a21046738ebc37
vkumar <vkumar@pocvm>
2022-05-30 17:40:48 +0000

    Add project files.

---
+++ APIGenerator/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Application.Controller
+{
+    class ApplicationAPIController
+    {
+
+        [HttpGet("/pets")]
+        public void Get
+        {
+
+        }
+
+        [HttpGet("/pets/{petId}")]
+        public void Get
+        {
+
+        }
+
+    }
+}
